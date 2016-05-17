bed2fc48ef968b4216f654554ff35f35f3bc7e18
Joshua Hunsberger <joshua@hunsberger.co>
2016-05-16 22:23:27 -0400

    feat: Add pet search function
    Add a function to search Petfinder's API for pets and
    return an array of JSON pet data.

--- MyPetFiles/PetfinderConvenience.swift
+++ MyPetFiles/PetfinderConvenience.swift
@@ -73,4 +73,78 @@ extension PetfinderClient {
             completionHandlerForSearchShelters(results: shelterArray, error: nil)
         }
     }
+    
+    /**
+     Function to search the Petfinder for adoptable pets given a set of parameters
+     
+     API Parameters and properties in ParameterKeys struct:
+     
+     Required:
+     - Developer Key: your developer key
+        - APIKey
+     - Location: the ZIP/postal code or city and state where the search should begin
+        - Location
+     
+     Optional:
+     - Token: session token
+        - SessionToken
+     - Animal: type of animal (barnyard, bird, cat, dog, horse, pig, reptile, smallfurry)
+        - Animal
+     - Breed: breed of animal (use breed.list for a list of valid breeds)
+        - Breed
+     - Size: size of animal (S=small, M=medium, L=large, XL=extra-large)
+        - Size
+     - Sex: M=male, F=female
+        - Sex
+     - Age: age of the animal (Baby, Young, Adult, Senior)
+        - Age
+     - Offset: offset into the result set (default is 0)
+        - ResultOffset
+     - Count: how many records to return for this particular API call (default is 25)
+        - RecordCount
+     - Output: How much of each record to return: basic (no description) or full (includes description)
+        - Output
+     - Format: Response format: xml, json
+        - ResultFormat
+     
+     - Parameters:
+     - parameters: The key value pairs to send to Petfinder.
+     - completionHandlerForSearchPets: The completion handler in which the results will be handled or errors processed.
+     */
+    func searchPets(parameters: [String: AnyObject], completionHandlerForSearchPets: (results: AnyObject!, error: NSError?) -> Void) {
+        func sendError(error: String){
+            let userInfo = [NSLocalizedDescriptionKey: error]
+            completionHandlerForSearchPets(results: nil, error: NSError(domain: "searchShelters", code: 1, userInfo: userInfo))
+        }
+        
+        taskForGETMethod(Methods.FindShelter, parameters: parameters) { (result, error) in
+            guard (error == nil) else {
+                sendError(error!.localizedDescription)
+                return
+            }
+            
+            guard let petfinder = result[JSONResponseKeys.Petfinder] as? [String: AnyObject] else {
+                sendError("Unable to retrieve petfinder key.")
+                return
+            }
+            
+            guard let pets = petfinder[JSONResponseKeys.Pets] as? [String: AnyObject] else {
+                sendError("Unable to retrieve pets key.")
+                return
+            }
+            
+            guard let petsArray = pets[JSONResponseKeys.Pet] as? [[String: AnyObject]] else {
+                sendError("Unable to retrieve pet array.")
+                return
+            }
+            
+            if petsArray.count == 0 {
+                sendError("No pets found. Try a different search.")
+                return
+            }
+            
+            // Call the completion handler with the array of pet JSON entries
+            completionHandlerForSearchPets(results: petsArray, error: nil)
+        }
+    }
 }
