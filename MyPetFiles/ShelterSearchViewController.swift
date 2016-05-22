12556414deda40e68b5e5d609b4756e67b450dc1
Joshua Hunsberger <joshua@hunsberger.co>
2016-05-21 21:24:41 -0400

    feat: Add ShelterSearchViewController
    Remove the generic ViewController class and add the ShelterSearchViewController
    to handler the shelter search view.

---
+++ MyPetFiles/ShelterSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ShelterSearchViewController.swift
+//  MyPetFiles
+//
+//  Created by Joshua Hunsberger on 5/21/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class ShelterSearchViewController: UIViewController {
+    
+}
