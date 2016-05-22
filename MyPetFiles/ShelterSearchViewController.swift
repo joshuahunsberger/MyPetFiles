52e52e6596ebd295dba11d727cafc6820ddfb8d6
Joshua Hunsberger <joshua@hunsberger.co>
2016-05-22 00:01:19 -0400

    feat: Add outlets and action
    Add outlets for the text fields and an IB action for the search button.
    If the location field is empty, display an alert to the user.

--- MyPetFiles/ShelterSearchViewController.swift
+++ MyPetFiles/ShelterSearchViewController.swift
@@ -10,4 +10,20 @@ import UIKit
 
 class ShelterSearchViewController: UIViewController {
     
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var groupNameTextField: UITextField!
+    
+    
+    @IBAction func searchButtonPressed(sender: AnyObject) {
+        if locationTextField.text!.isEmpty {
+            let errorTitle = "Missing Location"
+            let message = "Please enter a location in the top text box."
+            let alert = UIAlertController(title: errorTitle, message: message, preferredStyle: .Alert)
+            let dismissAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alert.addAction(dismissAction)
+            self.presentViewController(alert, animated: false, completion: nil)
+        } else {
+            // TODO: Initiate search based on text fields
+        }
+    }
 }
