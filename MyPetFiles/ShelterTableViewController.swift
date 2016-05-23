b81c17825e45527a963e7badadad566dc590fbf8
Joshua Hunsberger <joshua@hunsberger.co>
2016-05-22 22:13:21 -0400

    feat: Add ShelterTableViewController.swift
    Add a table view controller and a view controller class to show the details of
    the shelter search.

---
+++ MyPetFiles/ShelterTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ShelterTableViewController.swift
+//  MyPetFiles
+//
+//  Created by Joshua Hunsberger on 5/22/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class ShelterTableViewController: UITableViewController {
+    // MARK: Properties
+    var location: String!
+    var groupName: String?
+    
+    
+    // MARK: View Lifecycle Functions 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
