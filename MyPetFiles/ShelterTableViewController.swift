fb0c1175610d7588759f1042e19f1960b626c77c
Joshua Hunsberger <joshua@hunsberger.co>
2016-05-25 23:08:40 -0400

    feat: Populate shelter table with search results
    Query Petfinder's API for shelters and display the first page of results
    in a table view.

--- MyPetFiles/ShelterTableViewController.swift
+++ MyPetFiles/ShelterTableViewController.swift
@@ -12,10 +12,60 @@ class ShelterTableViewController: UITableViewController {
     // MARK: Properties
     var location: String!
     var groupName: String?
-    
+    var shelters = [Shelter]()
     
     // MARK: View Lifecycle Functions 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let loc = location, name = groupName {
+            // Set parameters
+            var parameters = [String: AnyObject]()
+            
+            parameters[PetfinderClient.ParameterKeys.Location] = loc
+            if (!name.isEmpty) {
+                parameters[PetfinderClient.ParameterKeys.Name] = name
+            }
+            
+            PetfinderClient.sharedInstance.searchShelters(parameters) { (results, error) in
+                if (error != nil) {
+                    // Display error
+                    // TODO: Display alert view for error
+                    print("\(error!.localizedDescription)")
+                } else {
+                    let sheltersJSON = results as! [[String: AnyObject]]
+                    
+                    for shelterJSON in sheltersJSON {
+                        let shelter = Shelter(dictionary: shelterJSON)
+                        self.shelters.append(shelter)
+                    }
+                    
+                    // Update the table on the main thread
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        } else {
+            // Search parameters were not set
+        }
+    }
+    
+    
+    // MARK: Table View Functions
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shelters.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let shelter = shelters[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("ShelterCell")!
+        cell.textLabel!.text = shelter.name
+        cell.detailTextLabel!.text = "\(shelter.city), \(shelter.state) \(shelter.zip)"
+        
+        return cell
     }
 }
