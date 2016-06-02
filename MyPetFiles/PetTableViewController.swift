89f94da83a71cda3acb165a4026943832993dea8
Joshua Hunsberger <joshua@hunsberger.co>
2016-06-01 22:59:09 -0400

    feat: Add Pet table view
    Add Pet search results table view to storyboard and add the requisite
    view controller class.

---
+++ MyPetFiles/PetTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PetTableViewController.swift
+//  MyPetFiles
+//
+//  Created by Joshua Hunsberger on 6/1/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class PetTableViewController: UITableViewController {
+    // MARK: Properties
+    var shelter: Shelter?
+    
+    
+    // MARK: View Lifecycle Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
