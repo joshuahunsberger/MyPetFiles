59bc420af2db9b8d6ec23f6efcde0724e474d90a
Joshua Hunsberger <joshua@hunsberger.co>
2016-06-03 18:13:57 -0400

    feat: Load pets for Shelter
    Add a function to load pets from Petfinder whenever a shelter is defined.

--- MyPetFiles/PetTableViewController.swift
+++ MyPetFiles/PetTableViewController.swift
@@ -11,11 +11,40 @@ import UIKit
 class PetTableViewController: UITableViewController {
     // MARK: Properties
     var shelter: Shelter?
+    var pets = [Pet]()
     
     
     // MARK: View Lifecycle Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let shelter = shelter {
+            getPets(shelter.id)
+        }
+    }
+    
+    
+    // MARK: UI Setup functions
+    
+    func getPets(shelterID: String) {
+        let parameters: [String: AnyObject] = [PetfinderClient.ParameterKeys.ID : shelterID]
+        
+        PetfinderClient.sharedInstance.getShelterPets(parameters) { (results, error) in
+            if (error != nil) {
+                // Handle error
+            } else {
+                let petsJSON = results as! [[String: AnyObject]]
+                
+                for petJSON in petsJSON {
+                    let pet = Pet(dictionary: petJSON)
+                    self.pets.append(pet)
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
 }
