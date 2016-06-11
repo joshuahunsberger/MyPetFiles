6efdb77060a3349e16e318e5bfa1c2d4838a70a2
Joshua Hunsberger <joshua@hunsberger.co>
2016-06-11 14:50:04 -0400

    feat: Display images from Petfinder
    Download the images for the given pet from Petfinder and displaying them in
    a UIScrollView with paging enabled.

--- MyPetFiles/PetDetailViewController.swift
+++ MyPetFiles/PetDetailViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class PetDetailViewController: UIViewController {
+    //MARK: Constants
+    let SCROLLVIEW_HEIGHT: CGFloat = 225.0
+    
+    
     // MARK: Properties
     var pet: Pet!
     
@@ -24,5 +28,43 @@ class PetDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         petNameLabel.text = pet.name
+        loadPhotos()
+    }
+    
+    
+    // MARK: UI setup functions
+    
+    func loadPhotos() {
+        photoScrollView.frame = CGRectMake(0, 0, view.frame.width, SCROLLVIEW_HEIGHT)
+        
+        let photoCount = pet.largePhotoURLs.count
+        photoScrollView.contentSize = CGSizeMake(photoScrollView.frame.width * CGFloat(photoCount), photoScrollView.frame.height)
+        photoScrollView.backgroundColor = UIColor.grayColor()
+        
+        for index in 1...photoCount {
+            let imageView = UIImageView(frame: CGRectMake(photoScrollView.frame.width * CGFloat(index-1), 0, photoScrollView.frame.width, photoScrollView.frame.height))
+            
+            let activityIndicator = UIActivityIndicatorView(frame: imageView.frame)
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = .WhiteLarge
+            activityIndicator.center = imageView.center
+            photoScrollView.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            
+            let url = NSURL(string: pet.largePhotoURLs["\(index)"] as! String)
+            
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
+                let data = NSData(contentsOfURL: url!)!
+                let image = UIImage(data: data)
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    activityIndicator.stopAnimating()
+                    imageView.image = image
+                    imageView.contentMode = .ScaleAspectFit
+                    self.photoScrollView.addSubview(imageView)
+                    
+                }
+            }
+        }
     }
 }
