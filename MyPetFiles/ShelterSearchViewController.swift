a6fc9dc29128672f0bddfc0fea9f74810b87e7c7
Joshua Hunsberger <joshua@hunsberger.co>
2016-06-20 08:11:48 -0400

    feat: Manage text views around keyboard
    Add functions to track keyboard notifications so the view can be moved
    to account for the keyboard.

--- MyPetFiles/ShelterSearchViewController.swift
+++ MyPetFiles/ShelterSearchViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ShelterSearchViewController: UIViewController {
+    // MARK: Properties
+    var selectedTextField: UITextField?
     // MARK: Interface Builder Outlet Properties
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var groupNameTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     
     // MARK: View Lifecycle Functions
@@ -22,6 +25,57 @@ class ShelterSearchViewController: UIViewController {
         groupNameTextField.delegate = self
         
         hideKeyboardWhenTappedAround()
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    
+    // MARK: Keyboard Notification Setup Functions
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWasShown), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillBeHidden), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications(){
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notification: NSNotification) {
+        guard let textField = selectedTextField else {
+            // No text field selected. Return
+            return
+        }
+        
+        if let userInfo = notification.userInfo {
+            if let keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+                let contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height, 0.0)
+                scrollView.contentInset = contentInsets
+                scrollView.scrollIndicatorInsets = contentInsets
+                
+                var aRect: CGRect = view.frame
+                aRect.size.height -= keyboardSize.height
+                if(!CGRectContainsPoint(aRect, textField.frame.origin)) {
+                    scrollView.scrollRectToVisible(textField.frame, animated: true)
+                }
+            }
+        }
+    }
+    
+    func keyboardWillBeHidden(notification: NSNotification) {
+        resetScrollView()
+    }
+    
+    func resetScrollView() {
+        // Scroll scrollview back to bottom
+        let contentInsets = UIEdgeInsetsZero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
     }
     
     
@@ -50,6 +104,10 @@ class ShelterSearchViewController: UIViewController {
 }
 
 extension ShelterSearchViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(textField: UITextField) {
+        selectedTextField = textField
+    }
+    
     // Dismiss keyboard when return is pressed
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
